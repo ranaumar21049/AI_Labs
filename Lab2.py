5b9b7f7ab2cf120d580e9b927fe0b890ce4adeca
ranaumar21049 <ranaumar8001@gmail.com>
2024-07-02 10:05:10 +0500

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,99 @@
+# count=0
+# while(count<3):
+#     count = count+1
+#     print("Hello Greek")
+
+
+
+# count=0    
+# while(count<3):count = count+1 ;print("Hello Greek")
+
+
+
+# print("List Iteration")
+# l = ["geeks", "for", "geeks"] 
+# for i in l:
+#     print(i)
+
+
+# print("\nTuple Iteration")
+# t = ("geeks", "for", "geeks") 
+# for i in t:
+#     print(i)
+
+
+
+# print("\nString Iteration") 
+# s = "Geeks"
+# for i in s :
+#     print(i)
+
+
+# Python program to illustrate 
+# Iterating by index
+# list = ["geeks", "for", "geeks"] 
+# for index in range(len(list)):
+#     print(list[index])
+
+
+
+# Prints all letters except 'e' and 's' 
+# for letter in 'geeksforgeeks':
+#     if letter == 'e' or letter == 's': 
+#         continue
+#     print('Current Letter :', letter )
+
+
+# for letter in 'geeksforgeeks':
+#     # break the loop as soon it sees 'e' 
+#     # or 's'
+#     if letter == 'e' or letter == 's': 
+#         break
+#     print('Current Letter :', letter )
+
+
+# def my_function():
+#     print("Hello from a function")
+# my_function()
+
+
+# def my_function(fname):
+#     print(fname + " Refsnes")
+
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+
+# def my_function(country = "Norway"): print("I am from " + country)
+# my_function("Sweden") 
+# my_function("India") 
+# my_function() 
+# my_function("Brazil")
+
+
+# def my_function(food):
+#     for x in food:
+#         print(x)
+# fruits = ["apple", "banana", "cherry"] 
+# my_function(fruits)
+
+
+# def my_function(x):
+#     return 5 * x
+# print(my_function(3))
+# print(my_function(5))
+# print(my_function(9))
+
+
+
+def my_function(child3, child2, child1):
+    print("The youngest child is " + child3)
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+
+
+
+
+
+
